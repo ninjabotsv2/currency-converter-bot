d97be53913d6af2bd9956cb6d0d0bde1e5e294f3
Sebastian Potekhin <seva.potekhin@icloud.com>
2019-05-29 23:38:51 +0300

    change api (#4)

--- bot/api/converter.py
+++ bot/api/converter.py
@@ -2,40 +2,36 @@ import os
 
 import requests
 
-SERVER_URL = 'https://free.currconv.com/api/v7'
+SERVER_URL = 'http://apilayer.net/api/'
 API_KEY = os.environ.get('API_KEY')
 
 
 class Converter:
-    def __init__(self):
-        self._currencies_query = None
-
     def convert(self, currency_from, currency_to, amount=1):
-        self._currencies_query = f"{currency_from}_{currency_to}".upper()
-        rate = self._send_convert_request()
+        rate = self._send_convert_request(currency_from, currency_to, amount)
 
         result = rate * amount
         return result
 
-    def _send_convert_request(self):
-        convert_url = f'{SERVER_URL}/convert'
+    def _send_convert_request(self, currency_from, currency_to, amount):
+        convert_url = f'{SERVER_URL}/live'
 
         response = requests.get(
             url=convert_url,
-            params=dict(
-                q=self._currencies_query,
-                apiKey=API_KEY
-            )
+            params={
+                'source': currency_from,
+                'access_key': API_KEY
+            }
         )
 
-        return self._get_conversion_result(response)
+        return self._get_conversion_result(response, currency_from, currency_to)
 
-    def _get_conversion_result(self, response):
+    def _get_conversion_result(self, response, currency_from, currency_to):
         if not response.json():
             raise Exception('No JSON in convert response')
 
         data = response.json()
-        if 'results' in data:
-            if self._currencies_query in data['results']:
-                return data['results'][self._currencies_query]['val']
-
+        if 'quotes' in data:
+            key = f"{currency_from}{currency_to}".upper()
+            if key in data['quotes']:
+                return data['quotes'][key]
